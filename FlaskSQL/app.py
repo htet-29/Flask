2c95e10cb38aeed4b9938963b100be5c357a3e90
Htet Hlaing Win <htethlaingwin.bsn29@gmail.com>
2024-10-03 22:27:48 +0630

    change

--- FlaskSQL/app.py
+++ FlaskSQL/app.py
@@ -1,3 +1,5 @@
+import os
+from flask_sqlalchemy import SQLAlchemy
 from flask import Flask, render_template, session, redirect, url_for, flash
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
@@ -5,12 +7,34 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 
+basedir = os.path.abspath(os.path.dirname(__file__))
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'haymar@2911999'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 bootstrap = Bootstrap(app)
 moment = Moment(app)
+db = SQLAlchemy(app)
 
+class Role(db.Model):
+    __tablename__ = 'roles'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True)
+    users = db.relationship('User', backref='role', lazy='dynamic')
+
+    def __repr__(self) -> str:
+        return '<Role %r>' % self.name 
+    
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), unique=True, index=True)
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+
+    def __repr__(self) -> str:
+        return '<User %r>' % self.username
 
 class NameForm(FlaskForm):
     name = StringField('What is your name?', validators=[DataRequired()])
--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # Flask
- 
+ Create virtual environment with
+python3 -m venv <name>
+source <name>/bin/activate
+
+pip install flask
+
+To use Database
+pip install flask-sqlalchemy
+
+to install requirements.txt file
+first pip freeze > requirements.txt
+pip install -r requirements.txt
+
